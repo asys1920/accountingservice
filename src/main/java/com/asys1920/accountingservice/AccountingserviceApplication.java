a35169b61f69fca9ad01c47dc3fb3e0848f4cc1c
Lars <lars.klippel@stud.th-bingen.de>
2019-12-06 16:31:08 +0100

    Spring Boot Initialization

---
+++ src/main/java/com/asys1920/accountingservice/AccountingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.asys1920.accountingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingserviceApplication.class, args);
+	}
+
+}
