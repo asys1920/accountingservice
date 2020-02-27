25f6a104427052f58615ea5c1dba3207f7698e4b
Thomas <thomas.hormes@hotmail.de>
2020-02-27 15:15:29 +0100

    Cleaned up AccountingService Pom; Added Dependencies to core Pom; Added Health Endpoint to AccountingService;

--- src/main/java/com/asys1920/service/AccountingServiceApplication.java
+++ src/main/java/com/asys1920/service/AccountingServiceApplication.java
@@ -12,9 +12,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class AccountingServiceApplication {
 
-	public AccountingServiceApplication() {
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccountingServiceApplication.class, args);
 	}
