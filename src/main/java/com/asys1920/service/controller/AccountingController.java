c309ceb4cbce0b9af73062dbe2cbc0a69d9e1cd1
AndreasKlar <andreas.klar@th-bingen.de>
2020-02-27 14:36:57 +0100

    Added Swagger Doc

--- src/main/java/com/asys1920/service/controller/AccountingController.java
+++ src/main/java/com/asys1920/service/controller/AccountingController.java
@@ -8,9 +8,7 @@ import com.asys1920.model.Balance;
 import com.asys1920.model.Bill;
 import com.asys1920.service.exceptions.ValidationException;
 import com.asys1920.service.service.AccountingService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +20,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+
+@Api(produces = "application/json")
 @RestController
 public class AccountingController {
 
@@ -32,9 +32,9 @@ public class AccountingController {
         this.accountingService = accountingService;
     }
 
-    @ApiOperation(value = "Create a bill", response = BillDTO.class)
+    @ApiOperation(value = "Create a new bill", response = BillDTO.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully created bill"),
+            @ApiResponse(code = 201, message = "Successfully created bill"),
             @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")})
@@ -51,7 +51,12 @@ public class AccountingController {
                 HttpStatus.CREATED);
     }
 
-
+    @ApiOperation(value = "Get all existing bills", response = BillDTO.class, responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully fetched bills"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden")
+    })
     @GetMapping("/bills")
     public ResponseEntity<List<BillDTO>> getAllBill() {
         List<Bill> allBills = accountingService.getAllBills();
@@ -61,6 +66,12 @@ public class AccountingController {
         return ResponseEntity.ok().body(allBillDTOs);
     }
 
+    @ApiOperation(value = "Get a existing bill", response = BillDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully fetched bill"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")})
     @GetMapping("/bills/{id}")
     public ResponseEntity<BillDTO> getBill(@PathVariable Long id) {
         Bill bill = accountingService.getBill(id);
@@ -69,6 +80,15 @@ public class AccountingController {
                 HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get the balance for a specific timeframe", response = BillDTO.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "start", value = "a timestamp when the balance should start", dataType = "Instant"),
+            @ApiImplicitParam(name = "end", value = "a timestamp when the balance should end", dataType = "Instant")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully fetched balance"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden")})
     @GetMapping("/balance")
     public ResponseEntity<BalanceDTO> getBalance(@RequestParam(required = false) Instant start,
                                                  @RequestParam(required = false) Instant end)
@@ -86,6 +106,12 @@ public class AccountingController {
                 HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Updates a specific Bill", response = BillDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated bill"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")})
     @PatchMapping("/bills/{id}")
     public ResponseEntity<BillDTO> updateBill(@RequestBody BillDTO billDTO) {
         Bill bill = accountingService.updateBill(BillMapper.INSTANCE.billDTOtoBill(billDTO));
